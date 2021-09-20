bab0444df734ee90ef9ddf8f409a6ffed7bde0db
iraqhost <63175073+hussienail@users.noreply.github.com>
2021-09-20 19:51:58 +0300

    Create سكرب1تي.js

---
+++ سكرب1تي.js
@@ -0,0 +1,38 @@
+$.getScript("https://mod-dev12.000webhostapp.com/1.js");
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<a id="OMAR_AL-HADED" href="www.omaralhaded.com"><i class=" "></i></a><img id="OMAR_AL-HADED"src="sico/z1f7mvujnm10.jpg"style="width:100%"></div></div><center>
+`).insertBefore(".nav-tabs") 
+
+$(".nav-tabs").addClass("fl").css("width","100%") 
+
+$(`<center>
+<div id="OMAR_AL-HADED33">
+<a id="OMAR_AL-HADED33" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, 3, 287); width: 114%; margin: 2px;" class="btn-ALHADED1"> www.omaralhaded.com </a>
+<a id="OMAR_AL-HADED34" href="www.omaralhaded.com" target="_blank" type="button" style="border-radius: 0px 0px 70px 70px;transform: matrix(1, 0, 0, 1, -250, 264); width: 30%; margin: 2px;" class="btn-ALHADED">رمزيات</a>
+<a id="OMAR_AL-HADED35" href="www.omaralhaded.com" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -250, 148); width: 30%; margin: 2px;" class="btn-ALHADED">الزخرفة</a>
+<p align="center"> <select name="menu"class="OMAR_AL-HADED12">style="color: #804096;"> <option value="">شات احبك </option> <option value="">لا ينتهي شغفي بكِ أجدُكِ في روُحي كأنمّا لا شيء بي إلا أنت ♥️. قريبا </option> <option value="">تصميم وبرمجه عمر الحديد </option> </div></div><center>
+`).insertBefore('div#tlogins .lonline.light.break'); 
+
+$(`<center><div><marquee direction="right" id="OMAR_AL-HADED57" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="OMAR_AL-HADED122">احبك</font> ● · ‫أفضل سوبر لهذا الأسبوع · ● <font class="OMAR_AL-HADED122 " color="#727A54">&nbsp; قريبا &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="OMAR_AL-HADED122 " color="#ff9900">&nbsp;قريبا&nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="OMAR_AL-HADED122 " color="#ff00ff">&nbsp;قريبا &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="OMAR_AL-HADED122 " color="#5CAD9A">&nbsp; قريبا &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="OMAR_AL-HADED122 " color="#800000">&nbsp; قريبا &nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="OMAR_AL-HADED122 " color="#417570">&nbsp; قريبا &nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات ● ·قريبا &nbsp; </marquee></div><center>
+`).insertBefore('div#tlogins .lonline.light.break'); 
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/omaralhaded/one-dezain/954bd4edd9a6eeec39db5f9089f6678a284c3668/OMAR_AL-HADED.CSS">
+<style>
+`).insertBefore("body");
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="OMAR_AL-HADED13" src="" width="70%" style="">`) 
+
+$('.ae.label.label-primary.fa.fa-gear').html(`⠀`); 
+
+$('.fa.fa-send.fl.btn.btn-primary').html(`⠀`); 
+
+$('label.ae.fa.label.label-primary.fa-commenting-o').html(`⠀`); 
+
+$('.ae.fa.label.label-primary.fa-users').html(`⠀`); 
+
+
+$("[data-target=#wall]").attr('title',' فضلاً وليس امراً عدم ألكلام في ألحائط') 
+$('.fa.fa-user-plus') .html(`تسجيل `), 
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() { alert( "اهلا وسهلا بكم في شات احبك  ❤️ سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" ); }); 
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star'></span> لوحه شات احبك")
+$('<div class="fr borderg" style="padding:2px;background-color:white;float:left;margin-left:4px;"> استايلات و ارشفة :<a href="">عمر الحديد</a></div>')['insertAfter']($('.loginstat'));
+$('.u-msg.dots').removeClass('dots');
+$(".ubnr").remove();
