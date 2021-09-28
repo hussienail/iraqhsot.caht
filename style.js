c6cb6b7a830aadf0d1fbdc973ff8ea96b500cf33
iraqhost <63175073+hussienail@users.noreply.github.com>
2021-09-28 16:14:00 +0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,210 @@
+																											try{
+
+
+
+$(`<a id="عراق-هوست-يحيى" href="">
+<i class=" "></i></a><mg id="asim27"src="<a id="asim27"target="_blank"type="a"rel="nofollow"title=" "href="ASIM"class="label-primary"style="margin:-2px 0 0 -374px"></a></div></div><center>`).insertBefore(".nav-tabs"),$(".nav-tabs").addClass("fl").css("width","100%"),
+$(`<center><div id="asim10">
+<div id="asim11 "><a id="asim10"target="_blank"type="a"href="rules.html"style="border-radius:70px 70px 0 0;transform:matrix(1,0,0,1,-256,12);width:100%;margin:2px;"class="btn minix btn-primary">القوانين</a>
+<div id="asim11 "><a id="asim11"target="_blank"type="a"href="rules.html"style="border-radius:70px 70px 0 0;transform:matrix(1,0,0,1,-256,12);width:100%;margin:4px;"class="btn minix btn-primary">أشتراكات</a>
+<div id="asim11 "><a id="asim12"target="_blank"type="a"href="rules.html"style="border-radius:70px 70px 0 0;transform:matrix(1,0,0,1,-256,12);width:100%;margin:4px;"class="btn minix btn-primary">ألتطبيق</a>
+<div id="asim11 "><a id="asim13"target="_blank"type="a"href="rules.html"style="border-radius:70px 70px 0 0;transform:matrix(1,0,0,1,-256,12);width:100%;margin:4px;"class="btn minix btn-primary">أتصل بنا</a>
+<a id="asim1" type="button" style="pointer-events:none;width:32%!important;height:20px;position:absolute;background:#8f6389;z-index:3;border:none!important;margin:0;top:380px;margin-left:-49px;border-left:2px solid #333!important;padding:1px 39px 0 0;font-family:'jazeera',FontAwesome;font-size:11px!important;color:#fefffc;" class="asim1">ADMIN ASIM</a>
+ </div></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<center><div><marquee direction="right" id="Yah3" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="Yah12">نجمه الحب</font> ? · ?أفضل سوبر لهذا الأسبوع · ? <font class="Yah12" color="#727A54">&nbsp; من يحفظ على احترامة امام الجميع &nbsp;</font> ? · ?أفضل زائر لهذا الأسبوع · ? <font class="Yah12" color="#ff9900">&nbsp;قريبا&nbsp;</font> ? · ?أفضل زائرة لهذا الأسبوع · ? <font class="Yah12" color="#ff00ff">&nbsp;قريبا&nbsp;</font> ? · مبدع الحائط لهذا الأسبوع · ? <font class="Yah12" color="#5CAD9A">&nbsp;  قريبا &nbsp;</font> ? · مبدعة الحائط لهذا الأسبوع · ? <font class="Yah12" color="#800000">&nbsp; قريبا &nbsp;</font> ? · ?أفضل تواجد لهذا الأسبوع · ? <font class="Yah12" color="#417570">&nbsp; قريبا  &nbsp;</font> ? · مع تمنياتنا للجميع بـ أطيب الاوقات ? ·  &nbsp;
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<link rel="stylesheet" href="https://raw.githack.com/hussienail/iraqhsot.caht/main/style3.css">
+<style>
+}
+.fl.btn.btn-primary {
+    border-radius: 0 0 30px 30px!important;
+    border-bottom: 0 solid#fff!important;
+    border-right: .5px solid#fff!important;
+    border-left: .5px solid#fff!important;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12),0 -3px 1px -2px rgba(0,0,0,.2)!important;
+    background-image: radial-gradient(circle,#484848,#888,#000)!important;
+    font-family: jazeera,FontAwesome;
+    font-size: 3px!important;
+}
+::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+}
+::-webkit-scrollbar-corner {
+    background: transparent;
+}
+::-webkit-scrollbar-thumb {
+    background: #050505;
+    border: 71px none #fff;
+    border-radius: 31px;
+    background-image: -webkit-linear-gradient( 
+45deg ,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important;
+}
+::-webkit-scrollbar-track {
+    background: #f8f9fa;
+    border: 65px none #fff;
+    border-radius: 34px;
+}
+div#dpnl.dpnl.bg.tab-content {
+    padding: 2px 5px;
+    border-radius: 10px;
+    box-shadow: 5px 4px 30px #000;
+    border: 3px solid hsla(0,0%,100%,.8588235294117647)!important;
+    background: hsla(0,0%,100%,.8588235294117647)!important;
+animation: fuck 1s !important;
+}
+ @keyframes fuck {0%{-webkit-transform:translate3d(100%,0,0);
+    {
+        {transform:translate3d(100%,0,0);visibility:visible }
+        to
+    }
+    {
+        -webkit-transform:translateZ(0);transform:translateZ(0)}
+    }
+}
+ul.nav.nav-tabs.fl li .fa-user-plus:before {
+    color: red;
+    content: "\f097";
+    -webkit-animation: mymove .5s infinite;
+    animation: mymove .5s infinite;
+}
+@-webkit-keyframes mymove {
+  from {
+    color:red;
+  }
+  to {
+    color:#FFF;
+  }
+}
+.label.tc.border.btn.fl {
+    cursor: pointer;
+    text-align: center;
+    padding-top: .3em;
+    border: 1px solid #000;
+    margin-top: 3px;
+    height: 32px;
+    font-weight: 700;
+    position: relative;
+    margin-bottom: .2rem;
+    border-radius: 40px 40px 0px 40px !important;
+    border: 1px solid #FFE0F6 !important;
+    font-family: 'jazeera',FontAwesome;
+border-radius: 0px 45px 37px 40px !important;
+    border: 1px solid #FFE0F6 !important;
+    background: linear-gradient( 
+-45deg , #cc31cc, #ff00ff2e, #635e63, #228eaf9e);
+    background-size: 400% 400%;
+    animation: asim 10s ease infinite;
+    Color: #fff;
+}
+   @keyframes asim {
+    0% {
+        background-position: 30% 0%;
+    }
+    50% {
+        background-position: 100% 20%;
+    }
+ 80% {
+        background-position: 30% 20%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+
+}
+div#dpnl.dpnl.bg.tab-content {
+    padding: 2px 5px;
+    border-radius: 10px;
+    box-shadow: 5px 4px 30px #000;
+    border: 3px solid hsla(0,0%,100%,.8588235294117647)!important;
+    background: hsla(0,0%,100%,.8588235294117647)!important;
+animation: fuck 1s !important;
+}
+ @keyframes fuck {0%{-webkit-transform:translate3d(100%,0,0);
+    {
+        {transform:translate3d(100%,0,0);visibility:visible }
+        to
+    }
+    {
+        -webkit-transform:translateZ(0);transform:translateZ(0)}
+    }
+}
+
+.u-pic.lazy.fitimg {
+    background-color: #fff;
+    border: 1px solid #dee2e6;
+    border-radius: .25rem;
+animation: seo 3.5s !important;
+    height: auto;
+    width: 158px;
+    height: 158px;
+    border-radius: 20px;
+    background-color: #ffffff00;
+    background-repeat: no-repeat;
+    background-image: linear-gradient(#41d8fd, #337caf), linear-gradient(#ea44ca, #8c1d8e), linear-gradient(#48fcfd, #b993bf), linear-gradient(#b084b7, #48fdfe);
+    -webkit-animation: rotateThis 3s linear infinite;
+    animation: rotateThis 3s linear infinite;
+}
+.room.borderg.hand.nosel.fl .fa-microphone:before {
+    margin: 0 0 0 -18px;
+    font-size: 20px;
+    color: #ff0a0a;
+     -webkit-animation: mymove .5s infinite; 
+     animation: mymove .5s infinite; 
+}
+@-webkit-keyframes mymove {
+  from {
+    color:red;
+  }
+  to {
+    color:#FFF;
+  }
+}
+</style>`).insertBefore("body");
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="Yah13" src="https://i.top4top.io/p_1982g29mx1.gif" width="70%" style="">`)
+$('.ae.label.label-primary.fa.fa-gear').html(` `);
+$('label.ae.fa.label.label-primary.fa-commenting-o').html(` `);
+$('.ae.fa.label.label-primary.fa-users').html(` `);
+$('.fa.fa-send.fl.btn.btn-primary').html(` `);
+$('.fa.fa-send.sndbc.fl.btn.btn-primary').html(` `);
+$(document.getElementById("tbox").placeholder = "  حيـأكم شـأت نجمه الحب ");
+$("[data-target=#wall]").attr('title',' ابداعات الحايط')
+$("[data-target=#settings]").attr('title','اعدادات')
+$("[data-target=#rooms]").attr('title','الغرف')
+$("[data-target=#chats]").attr('title','الخاص')
+$("[data-target=#users]").attr('title','المتواجدين')
+$('.fl.ustat')
+    .css('width', '3px'), $('.fa.fa-user-plus')
+    .html(`تسجيل `), document.getElementById('u1')
+    .placeholder = 'اكتب اسمك من 5 احرف فقط ', 
+document.getElementById('pass1')
+  .placeholder = 'كلمة المرور', document.getElementById('u2')
+    .placeholder = 'فخأمة طلتك تنورنا', document.getElementById('pass2')
+    .placeholder = ' ? بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+    .placeholder = '? أكتب اسمك ولازم تحفظه ';
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+alert( "اهلا وسهلا بكم في شات نجمه الحب   سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
+});
+
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم نجمه الحب ")
+
+$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> إعلانات نجمه الحب للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
+
+$(`<img src="/site/1632168440085.gif"class="img asim aetar"style="height: 26%;
+width:100%;
+margin-top: 1px;
+background-color: white;
+border-radius: 10px;
+border: 2px dashed#544d4d">`).insertBefore('.nav-tabs');
+
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>omar alhaded </span></a>-->
+<a href="ASIM10" target="_blank" style="width:107px; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+border-color: #990033;
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >Style By: ASIM</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+}
+													catch (e){
+															console.log(e)
+														}
+("عمر_ الديوث _ سارق سكربتات _ كس امك _ يا _ ابن _ قحبه_ يا ديوث _ اردنين حراميه _ كس ام اردن _ كسختك باقر الفرخ عمرك 13 سنه وطب شاتات ابن كحبه وصاير مبرمج براسي وتبوك سكربتاتي وكواد مالاتي ابن زانيه الف عير بامك ^_^")
