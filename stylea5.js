a8ed1506dc74ed328c524fe70659fb18d41315b2
iraqhost <63175073+hussienail@users.noreply.github.com>
2021-10-05 19:34:21 +0300

    Update and rename stylea4.js to stylea5.js

--- stylea5.js
+++ stylea5.js
@@ -2,6 +2,10 @@ $(`<style font="http://fonts.googleapis.com/css?family=Amiri&subset=arabic,latin
 a.label.label-primary.fl{padding:5px!important;margin-bottom:2px;text-align:center!important;background-image:radial-gradient(circle,#000000,#000000,#ff001466)!important;font-family:jazeera,FontAwesome}
 ul.nav.nav-tabs>li>a{color:#fefffc;background:0 0;margin:auto;text-align:center;width:100%;border-radius:7px;margin-top:2px;font-family:jazeera,FontAwesome;border-right:2px ridge!important;border-left:3px ridge!important;background-image:linear-gradient( 
 90deg,#ff0014ba 0,#000000 10%,#000000 51%,#000000 89%,#ff0014ba 100%);border:1px solid #f0ad4e;background-color:inherit!important;height:32px;text-align:center;border-radius:6px;margin:2px 0;padding:4px 0!important;width:113px!important;margin-left:5px!important;font-family:'jazeera',FontAwesome}
+div#l1 button{display:block;width:50%;margin-left:90px;padding:1px 2px;text-align:center;border:none;background:linear-gradient( 
+90deg,#ff00148a 0,#000000 10%,#000000 51%,#000000 89%,#ff00148a 100%);top:74px;border-radius:4px;font-size:.9rem;color:#fff;cursor:pointer;transition:.3s}
+div#l2 button,div#l3 button{position:absolute;width:50%;margin-left:90px;padding:1px 2px;text-align:center;border:none;background:linear-gradient( 
+90deg,#ff00148a 0,#000000 10%,#000000 51%,#000000 89%,#ff00148a 100%);top:74px;border-radius:4px;font-size:.9rem;color:#fff;cursor:pointer;transition:.3s}
 </style>`).insertBefore('body');
 $('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '0px solid #ffffff',borderLeft: '0px solid #ffffff',borderTop: '.0px solid #ffffff',borderBottom: '.0px solid #ffffff',paddingTop: '3px',borderRadius: '0px 0px 0px 0px'});
 $('.nav-tabs').addClass('fl').css('width','100%');
