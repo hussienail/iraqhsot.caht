c3c164702dee7811381fe5daa8ebd6107f404a79
iraqhost <63175073+hussienail@users.noreply.github.com>
2021-10-05 19:44:18 +0300

    Update and rename stylea7.js to stylea8.js

---
+++ stylea8.js
@@ -0,0 +1,58 @@
+$(`<style font="http://fonts.googleapis.com/css?family=Amiri&subset=arabic,latin"> 
+a.label.label-primary.fl{padding:5px!important;margin-bottom:2px;text-align:center!important;background-image:radial-gradient(circle,#000000,#000000,#ff001466)!important;font-family:jazeera,FontAwesome}
+ul.nav.nav-tabs>li>a{color:#fefffc;background:0 0;margin:auto;text-align:center;width:100%;border-radius:7px;margin-top:2px;font-family:jazeera,FontAwesome;border-right:2px ridge!important;border-left:3px ridge!important;background-image:linear-gradient( 
+90deg,#ff0014ba 0,#000000 10%,#000000 51%,#000000 89%,#ff0014ba 100%);border:1px solid #f0ad4e;background-color:inherit!important;height:32px;text-align:center;border-radius:6px;margin:2px 0;padding:4px 0!important;width:113px!important;margin-left:5px!important;font-family:'jazeera',FontAwesome}
+div#l1 button{display:block;width:50%;margin-left:90px;padding:1px 2px;text-align:center;border:none;background:linear-gradient( 
+90deg,#ff00148a 0,#000000 10%,#000000 51%,#000000 89%,#ff00148a 100%);top:74px;border-radius:4px;font-size:.9rem;color:#fff;cursor:pointer;transition:.3s}
+div#l2 button,div#l3 button{position:absolute;width:110px;margin-top:0;padding:1px 2px;text-align:center;border:none!important;background:linear-gradient( 
+90deg,#ff00148a 0,#000000 10%,#000000 51%,#000000 89%,#ff00148a 100%);outline:none;border-radius:4px;font-size:.9rem;color:#fff;cursor:pointer;transition:.3s;font-family:jazeera,FontAwesome}
+</style>`).insertBefore('body');
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '0px solid #ffffff',borderLeft: '0px solid #ffffff',borderTop: '.0px solid #ffffff',borderBottom: '.0px solid #ffffff',paddingTop: '3px',borderRadius: '0px 0px 0px 0px'});
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<style>
+div.hand.corner {
+-webkit-animation: swoopOut 3s 1 ease forwards;
+background-image: radial-gradient(circle,#484848,#888,#000)!important;
+    font-family: jazeera,FontAwesome;
+    font-size: 11px!important;
+    text-align: center;
+    color: #ffffff;
+    min-width: 180px;
+    max-width: 260px;
+    border: 1px solid #f8f9fa;
+    z-index: 2000;
+    background-color: #efefef;
+    position: absolute;
+    top: 30%;
+    padding: 5px;
+    border-radius: 5px;
+    margin-bottom: -.2rem;
+    left: 15px
+} 
+
+@-webkit-keyframes swoopOut {
+  100% {
+    position:absolute;
+    left:50px;
+    visibility:hidden;
+  }
+  24% {
+    position:absolute;
+    left:50px;
+  }
+.label-warning {
+    background-color: #f0ad4e;
+}
+
+span#name{
+    position: relative;
+    color: #fff;
+    display:block;
+    top: 45px;
+    font-size: 12px !important;
+    background: #000;
+    top: 68px!important;
+    margin: -24px;
+    border: 2px solid #dc3545;
+}
+</style>`).insertBefore("body");
