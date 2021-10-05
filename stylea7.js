869ac4d0019ca456204f09e53d094501521e0e74
iraqhost <63175073+hussienail@users.noreply.github.com>
2021-10-05 19:39:31 +0300

    Update and rename stylea6.js to stylea7.js

--- stylea7.js
+++ stylea7.js
@@ -4,8 +4,8 @@ ul.nav.nav-tabs>li>a{color:#fefffc;background:0 0;margin:auto;text-align:center;
 90deg,#ff0014ba 0,#000000 10%,#000000 51%,#000000 89%,#ff0014ba 100%);border:1px solid #f0ad4e;background-color:inherit!important;height:32px;text-align:center;border-radius:6px;margin:2px 0;padding:4px 0!important;width:113px!important;margin-left:5px!important;font-family:'jazeera',FontAwesome}
 div#l1 button{display:block;width:50%;margin-left:90px;padding:1px 2px;text-align:center;border:none;background:linear-gradient( 
 90deg,#ff00148a 0,#000000 10%,#000000 51%,#000000 89%,#ff00148a 100%);top:74px;border-radius:4px;font-size:.9rem;color:#fff;cursor:pointer;transition:.3s}
-div#l2 button,div#l3 button{position:absolute;width:50%;margin-left:90px;padding:1px 2px;text-align:center;border:none;background:linear-gradient( 
-90deg,#ff00148a 0,#000000 10%,#000000 51%,#000000 89%,#ff00148a 100%);border-radius:4px;font-size:.9rem;color:#fff;cursor:pointer;transition:.3s}
+div#l2 button,div#l3 button{position:absolute;width:110px;margin-top:0;padding:1px 2px;text-align:center;border:none!important;background:linear-gradient( 
+90deg,#ff00148a 0,#000000 10%,#000000 51%,#000000 89%,#ff00148a 100%);outline:none;border-radius:4px;font-size:.9rem;color:#fff;cursor:pointer;transition:.3s;font-family:jazeera,FontAwesome}
 </style>`).insertBefore('body');
 $('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '0px solid #ffffff',borderLeft: '0px solid #ffffff',borderTop: '.0px solid #ffffff',borderBottom: '.0px solid #ffffff',paddingTop: '3px',borderRadius: '0px 0px 0px 0px'});
 $('.nav-tabs').addClass('fl').css('width','100%');
